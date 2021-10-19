e9e7216809a78a703a50ad25c0dba00ff8f67938
Seung Ki Lee <seungkilee92@gmail.com>
2021-10-18 22:56:56 -0500

    finished tutorial, fun stuff

---
+++ src/example/hello.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"math"
+)
+
+func sum(x int, y int) int {
+	return x + y
+}
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Square Root Undefined for Negative Number")
+	} else {
+		return math.Sqrt(x), nil
+	}
+}
+
+// Class Struct
+type person struct {
+	name string
+	age  int
+}
+
+func main() {
+	p1 := person{name: "Seung Ki", age: 28}
+
+	// make for map, delete for value of the map
+	m := make(map[string]int)
+	m["a"] = 1
+	m["b"] = 2
+	m["sum"] = sum(m["a"], m["b"])
+	float := -4.0
+	sqrt_float, err := sqrt(float)
+
+	// loop over maps!
+	for key, value := range m {
+		fmt.Println("key: ", key, "value: ", value)
+	}
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(sqrt_float)
+	}
+
+	fmt.Print(p1)
+
+	// PTR for pass by ref
+	ptr := 7
+	inc(&ptr)
+	fmt.Println(ptr)
+
+}
+
+func inc(x *int) {
+	*x++
+}
